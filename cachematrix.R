5ccef30b1c95c499e1b7ae7a018e6226bafa48fa
Srikiran <sriki_zoro@hotmail.com>
2016-10-05 09:44:18 +0530

    Compute Inverse
    Modified the code to compute the inverse of a matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- matrix(nrow = dim(x)[1], ncol = dim(x)[2])
+    set <- function(y) {
+        x <<- y
+        i <<- matrix(nrow = dim(x)[1], ncol = dim(x)[2])
+    }
+    get <- function() x
+    setInverse <- function(inverse) i <<- inverse
+    getInverse <- function() i
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
+    
+    
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    i <- x$getInverse()
+    if(!anyNA(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setInverse(i)
+    i
 }
